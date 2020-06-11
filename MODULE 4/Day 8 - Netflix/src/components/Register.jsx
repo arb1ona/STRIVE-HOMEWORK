926af581acefdb795205dda97048257c8bfd10d8
Arbiona <63557021+arb1ona@users.noreply.github.com>
2020-06-11 17:21:13 +0200

    Update Register Form

--- MODULE 4/Day 8 - Netflix/src/components/Register.jsx
+++ MODULE 4/Day 8 - Netflix/src/components/Register.jsx
@@ -1,96 +1,68 @@
-import React from "react";
+import React from 'react'
+import { Form, Button, Col, Alert } from 'react-bootstrap'
 
-const initialState = {
-    name: "",
-    email: "",
-    password: "",
-    nameError: "",
-    emailError: "",
-    passwordError: ""
-};
-
-export default class Register extends React.Component {
-    state = initialState;
-
-    handleChange = event => {
-        const isCheckbox = event.target.type === "checkbox";
-        this.setState({
-            [event.target.name]: isCheckbox
-                ? event.target.checked
-                : event.target.value
-        });
-    };
-
-    validate = () => {
-        let nameError = "";
-        let emailError = "";
-        // let passwordError = "";
-
-        if (!this.state.name) {
-            nameError = "name cannot be blank";
-        }
-
-        if (!this.state.email.includes("@")) {
-            emailError = "invalid email";
-        }
-
-        if (emailError || nameError) {
-            this.setState({ emailError, nameError });
-            return false;
-        }
-
-        return true;
-    };
-
-    handleSubmit = event => {
-        event.preventDefault();
-        const isValid = this.validate();
-        if (isValid) {
-            console.log(this.state);
-            // clear form
-            this.setState(initialState);
-        }
-    };
+class Register extends React.Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <div>
-                    <input
-                        name="name"
-                        placeholder="name"
-                        value={this.state.name}
-                        onChange={this.handleChange}
-                    />
-                    <div style={{ fontSize: 12, color: "red" }}>
-                        {this.state.nameError}
-                    </div>
-                </div>
-                <div>
-                    <input
-                        name="email"
-                        placeholder="email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                    />
-                    <div style={{ fontSize: 12, color: "red" }}>
-                        {this.state.emailError}
-                    </div>
-                </div>
-                <div>
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                    />
-                    <div style={{ fontSize: 12, color: "red" }}>
-                        {this.state.passwordError}
-                    </div>
-                </div>
-                <button type="submit">submit</button>
-            </form>
-        );
+            <Form>
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Control type="text" required pattern="[A-Za-z]{2, }" placeholder="Name" />
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
+                        <Form.Control type="text" required pattern="[A-Za-z]{3, }" placeholder="Last Name" />
+                    </Form.Group>
+                </Form.Row>
+
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Control type="email" required placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
+                        <Form.Control type="password" required pattern="[A-Za-z!]{8, }" placeholder="Password" />
+                    </Form.Group>
+                </Form.Row>
+
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Control required placeholder="Year of birth" />
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
+                        <Form.Control required placeholder="Street Adress" />
+                    </Form.Group>
+
+                </Form.Row>
+
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Control as="select" defaultValue="Choose City" >
+                            <option>Choose City</option>
+                            <option>...</option>
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
+                        <Form.Control required placeholder="Postal Code" />
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
+                        <Form.Control required placeholder="City" />
+                    </Form.Group>
+                </Form.Row>
+
+                <Form.Row >
+                    <Button variant="primary" type="submit" class="btn">
+                        Submit
+                    </Button>
+                </Form.Row>
+            </Form>
+        )
     }
+
 }
+
+export default Register;
