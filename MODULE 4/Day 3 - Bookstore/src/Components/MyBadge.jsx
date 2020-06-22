40ed261ccc5036383a07fac24ca385ad04294664
Arbiona <63557021+arb1ona@users.noreply.github.com>
2020-06-22 23:40:46 +0200

    Update MyBadge.jsx

--- MODULE 4/Day 3 - Bookstore/src/Components/MyBadge.jsx
+++ MODULE 4/Day 3 - Bookstore/src/Components/MyBadge.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Badge } from "react-bootstrap";
 
 function MyBadge() {
-
     return (
         <div>
             <h5>My Badge<Badge variant="secondary">New</Badge></h5>
